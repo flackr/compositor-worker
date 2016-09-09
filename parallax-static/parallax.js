483e71f897eb4a20183703fdd33a8cd22a83277f
Robert Flack <flackr@gmail.com>
2016-09-09 14:36:02 +0000

    Fix static parallax demo by using overflow scroller.

--- parallax-static/parallax.js
+++ parallax-static/parallax.js
@@ -4,25 +4,37 @@
   scope.isMain = function() {
     return scope.window;
   };
+  var lastScroll = 1;
 
   scope.tick = function(timestamp) {
-    console.log("tick:"+timestamp);
+    if (!scroller.initialized || !bg.initialized || !bg2.initialized || !bg3.initialized || scroller.scrollTop == lastScroll) {
+      scope.requestAnimationFrame(tick);
+      return;
+    }
+    lastScroll = scroller.scrollTop;
+    self.postMessage('Scroll is at ' + lastScroll);
     var t = bg.transform;
     t.m42 = Math.round(scroller.scrollTop*0.8);
     bg.transform = t;
     var t2 = bg2.transform;
     t2.m42 = Math.round(scroller.scrollTop*0.6);
     bg2.transform = t2;
+    
+    var t3 = bg3.transform;
+    t3.m42 = Math.round(scroller.scrollTop);
+    bg3.transform = t3;
     scope.requestAnimationFrame(tick);
   };
 
   scope.initWorker = function() {
-    console.log("init worker");
+    self.postMessage('worker is alive');
 
     self.onmessage = function(e) {
       scope.scroller = e.data[0];
       scope.bg = e.data[1];
       scope.bg2 = e.data[2];
+      scope.bg3 = e.data[3];
+      self.postMessage('proxies received');
 
       scope.requestAnimationFrame(tick);
     };
@@ -33,11 +45,15 @@
 
     scope.worker = new CompositorWorker("parallax.js");
 
-    scope.scroller = new CompositorProxy(document.scrollingElement, ['scrollTop']);
+    scope.scroller = new CompositorProxy(document.getElementById("scroller"), ['scrollTop']);
     scope.bg = new CompositorProxy(document.getElementById("bg"), ['transform']);
     scope.bg2 = new CompositorProxy(document.getElementById("bg2"), ['transform']);
-
-    worker.postMessage([scroller, bg, bg2]);
+    scope.bg3 = new CompositorProxy(document.getElementById("stuck"), ['transform']);
+    
+    worker.postMessage([scroller, bg, bg2, bg3]);
+    worker.onmessage = function(e) {
+      console.log('Worker said: ' + e.data);
+    }
   };
 
   if (isMain())
